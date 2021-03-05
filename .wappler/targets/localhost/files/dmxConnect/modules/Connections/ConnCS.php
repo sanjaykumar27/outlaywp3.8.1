93ef450c6c659e1efedde305ef51e7fbd4ccbb14
SLASHASH-05\yo <sanjaychaurasia@slashash.co>
2021-03-05 17:53:14 +0530

    no message

---
+++ .wappler/targets/localhost/files/dmxConnect/modules/Connections/ConnCS.php
@@ -0,0 +1,15 @@
+<?php
+$exports = <<<'JSON'
+{
+  "name": "ConnCS",
+  "module": "dbconnector",
+  "action": "connect",
+  "options": {
+    "server": "mysql",
+    "databaseType": "MySQL",
+    "connectionString": "mysql:host=localhost;sslverify=false;dbname=mindfuli_expense;user=root;charset=utf8",
+    "meta": false
+  }
+}
+JSON;
+?>
