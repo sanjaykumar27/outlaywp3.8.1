b30403fd925ec905a3fd95e32c99f9b6452f5879
SLASHASH-05\yo <sanjaychaurasia@slashash.co>
2021-03-05 18:00:27 +0530

    no message

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,7 @@
 /* checking if browser supports service worker, if yes then registor */
 
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    window.location = "http://localhost/exp/mobile-login";
+    window.location = "http://152.67.160.39/exp/mobile-login";
 }
 
 function MonthlyGraph() {
