1d5c9071d698cb5c20a08c9cfd2802d057e0f422
SLASHASH-05\yo <sanjaychaurasia@slashash.co>
2021-03-06 17:57:17 +0530

    no message

--- mobileui/login.php
+++ mobileui/login.php
@@ -2,89 +2,94 @@
 <html>
 
 <head>
-  <meta name="ac:route" content="/mobile-login">
-  <meta name="ac:base" content="/exp">
-  <base href="/exp/mobileui/">
-  <script src="../dmxAppConnect/dmxAppConnect.js"></script>
-  <meta charset="UTF-8">
-  <title>Mobile Login</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="theme-color" content="#0134d4">
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;1,300;1,400;1,600&display=swap" rel="stylesheet">
-  <meta name="apple-mobile-web-app-capable" content="yes">
-  <meta name="apple-mobile-web-app-status-bar-style" content="black">
-  <link rel="stylesheet" href="../bootstrap/5/css/bootstrap.min.css" />
-  <link rel="stylesheet" href="../fontawesome5/css/all.min.css" />
-  <script src="../js/jquery-3.5.1.slim.min.js"></script>
-  <script src="../js/moment.js/2/moment-with-locales.min.js"></script>
-  <link rel="icon" href="img/core-img/favicon.ico">
-  <link rel="apple-touch-icon" href="img/icons/icon-96x96.png">
-  <link rel="apple-touch-icon" sizes="152x152" href="img/icons/icon-152x152.png">
-  <link rel="apple-touch-icon" sizes="167x167" href="img/icons/icon-167x167.png">
-  <link rel="apple-touch-icon" sizes="180x180" href="img/icons/icon-180x180.png">
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/animate.css">
-  <link rel="stylesheet" href="css/owl.carousel.min.css">
-  <link rel="stylesheet" href="css/font-awesome.min.css">
-  <link rel="stylesheet" href="css/magnific-popup.css">
-  <link rel="stylesheet" href="css/ion.rangeSlider.min.css">
-  <link rel="stylesheet" href="css/dataTables.bootstrap4.min.css">
-  <link rel="stylesheet" href="css/apexcharts.css">
-  <!-- Core Stylesheet-->
-  <link rel="stylesheet" href="css/style.css">
-  <link rel="stylesheet" href="css/custom.css">
-  <!-- Web App Manifest-->
-  <link rel="manifest" href="manifest.json">
+    <meta name="ac:route" content="/mobile-login">
+    <meta name="ac:base" content="/exp">
+    <base href="/exp/mobileui/">
+    <script src="../dmxAppConnect/dmxAppConnect.js"></script>
+    <meta charset="UTF-8">
+    <title>Outlay Mobile Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="theme-color" content="#0134d4">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;1,300;1,400;1,600&display=swap" rel="stylesheet">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <link rel="stylesheet" href="../bootstrap/5/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="../fontawesome5/css/all.min.css" />
+    <script src="../js/jquery-3.5.1.slim.min.js"></script>
+    <script src="../js/moment.js/2/moment-with-locales.min.js"></script>
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <link rel="stylesheet" href="../dmxAppConnect/dmxNotifications/dmxNotifications.css" />
+    <script src="../dmxAppConnect/dmxNotifications/dmxNotifications.js" defer=""></script>
+    <script src="../dmxAppConnect/dmxBrowser/dmxBrowser.js" defer=""></script>
 </head>
 
 <body is="dmx-app" id="login">
-  <div class="internet-connection-status" id="internetStatus"></div>
-  <!-- Login Wrapper Area-->
-  <div class="login-wrapper d-flex align-items-center justify-content-center">
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-12 col-sm-9 col-md-7 col-lg-6 col-xl-5">
-          <div class="text-center px-4"><img class="login-intro-img" src="img/bg-img/36.png" alt=""></div>
-          <!-- Register Form-->
-          <div class="register-form mt-4 px-4">
-            <form action="https://designing-world.com/affan-1.0/page-home.html">
-              <div class="form-group text-start mb-3">
-                <input class="form-control" type="text" placeholder="Username or email address">
-              </div>
-              <div class="form-group text-start mb-3">
-                <input class="form-control" type="password" placeholder="Enter Password">
-              </div>
-              <button class="btn btn-primary w-100" type="submit">Sign In</button>
-            </form>
-          </div>
-          <!-- Login Meta-->
-          <!-- <div class="login-meta-data text-center"><a class="stretched-link forgot-password d-block mt-3 mb-1" href="page-forget-password.html">Forgot Password?</a>
-              <p class="mb-0">Didn't have an account?<a class="ms-1 stretched-link" href="page-register.html">Register Now</a></p>
-            </div> -->
+    <div is="dmx-browser" id="browser1"></div>
+    <dmx-notifications id="notifies1"></dmx-notifications>
+
+
+    <div id="appCapsule">
+        <div class="section mt-2 text-center">
+            <h1>OUTLAY</h1>
+            <h4>Log In</h4>
         </div>
-      </div>
+        <div class="section mb-5 p-2">
+            <form novalidate="novalidate" id="auth-form" is="dmx-serverconnect-form" action="../dmxConnect/api/AccessControl/Login.php" dmx-on:success="notifies1.success('Succesfully Logged In');browser1.goto('mobile-dashboard.php')"
+                dmx-on:unauthorized="notifies1.danger('Invalid Login')" method="post">
+                <div class="card">
+                    <div class="card-body pb-1">
+                        <div class="form-group basic">
+                            <div class="input-wrapper">
+                                <label class="label" for="email1">Username</label>
+                                <input type="text" class="form-control" autocomplete="off" id="email1" name="username" placeholder="Username">
+                                <i class="clear-input">
+                                    <ion-icon name="close-circle"></ion-icon>
+                                </i>
+                            </div>
+                        </div>
+
+                        <div class="form-group basic">
+                            <div class="input-wrapper">
+                                <label class="label" for="password1">Password</label>
+                                <input type="password" class="form-control" name="password" id="password1" placeholder="Password">
+                                <i class="clear-input">
+                                    <ion-icon name="close-circle"></ion-icon>
+                                </i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="form-links mt-2">
+                    <div>
+                        <a href="app-register.html">Register Now</a>
+                    </div>
+                    <div><a href="app-forgot-password.html" class="text-muted">Forgot Password?</a></div>
+                </div>
+
+                <div class="form-button-group  transparent">
+                    <button type="submit" class="btn btn-primary btn-block btn-lg">Log in</button>
+                </div>
+
+            </form>
+        </div>
+
     </div>
-  </div>
-  <script src="../bootstrap/5/js/bootstrap.bundle.min.js"></script>
-  <!-- <script src="js/bootstrap.bundle.min.js"></script> -->
-  <script src="js/default/internet-status.js"></script>
-  <script src="js/waypoints.min.js"></script>
-  <script src="js/jquery.easing.min.js"></script>
-  <script src="js/wow.min.js"></script>
-  <script src="js/owl.carousel.min.js"></script>
-  <script src="js/jquery.counterup.min.js"></script>
-  <script src="js/jquery.countdown.min.js"></script>
-  <script src="js/imagesloaded.pkgd.min.js"></script>
-  <script src="js/isotope.pkgd.min.js"></script>
-  <script src="js/jquery.magnific-popup.min.js"></script>
-  <script src="js/default/dark-mode-switch.js"></script>
-  <script src="js/ion.rangeSlider.min.js"></script>
-  <script src="js/jquery.dataTables.min.js"></script>
-  <script src="js/default/active.js"></script>
-  <script src="js/default/clipboard.js"></script>
-  <!-- PWA-->
-  <script src="js/pwa.js"></script>
+
+    <!-- Bootstrap-->
+    <script src="assets/js/lib/popper.min.js"></script>
+    <script src="assets/js/lib/bootstrap.min.js"></script>
+    <!-- Ionicons -->
+    <script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
+    <!-- Owl Carousel -->
+    <script src="assets/js/plugins/owl-carousel/owl.carousel.min.js"></script>
+    <!-- Base Js File -->
+
+
+    <script src="../bootstrap/5/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
