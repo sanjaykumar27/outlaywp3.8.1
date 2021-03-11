731e55bdf45d850b1092303dcc4757f4931aa35b
Sanjay Kumar <engineer.sanjay20@gmail.com>
2021-03-11 21:12:21 +0530

    no message

--- dmxConnect/api/Dashboard/getMonthlyExpense.php
+++ dmxConnect/api/Dashboard/getMonthlyExpense.php
@@ -6,8 +6,6 @@ $app = new \lib\App();
 
 $app->define(<<<'JSON'
 [
-  "Connections/ConnCS",
-  "SecurityProviders/SecurityCS",
   {
     "name": "",
     "module": "auth",
@@ -26,7 +24,7 @@ $app->define(<<<'JSON'
     "options": {
       "connection": "ConnCS",
       "sql": {
-        "query": "SELECT `id`, SUM(amount) as amount, DATE_FORMAT(purchase_date, \"%b %Y\") as dates FROM `expense` GROUP BY MONTH(purchase_date), YEAR(purchase_date) order by purchase_date desc LIMIT 12",
+        "query": "SELECT `id`, SUM(amount) as amount, purchase_date, DATE_FORMAT(purchase_date, \"%b %Y\") as dates FROM `expense` GROUP BY MONTH( purchase_date, , YEAR(purchase_date) order by purchase_date desc LIMIT 12",
         "params": []
       }
     },
