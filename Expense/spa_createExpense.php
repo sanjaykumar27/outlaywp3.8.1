0409dd5e6d7c333fa46bf0ffc0f260a3d513bdd4
SLASHASH-05\yo <sanjaychaurasia@slashash.co>
2021-03-24 17:35:46 +0530

    expense

--- Expense/spa_createExpense.php
+++ Expense/spa_createExpense.php
@@ -6,6 +6,9 @@
 <dmx-datetime id="varDateTime"></dmx-datetime>
 <dmx-notifications id="notifies1" offset-x="30" offset-y="30" closable newest-on-top></dmx-notifications>
 <dmx-value id="varCounter" dmx-bind:value="1"></dmx-value>
+<dmx-value id="varRemark" dmx-bind:value="0"></dmx-value>
+<dmx-value id="varInvoiceName" dmx-bind:value="0"></dmx-value>
+<dmx-value id="varAttachment" dmx-bind:value="0"></dmx-value>
 <dmx-serverconnect id="scExpenseList" url="dmxConnect/api/Expense/ExpenseList.php" dmx-param:offset="query.offset" dmx-param:limit="10" dmx-param:currentmonth="0" dmx-param:date="" dmx-param:newexpense="1">
 </dmx-serverconnect>
 <div class="subheader py-2 py-lg-4 subheader-solid" id="kt_subheader">
@@ -16,9 +19,10 @@
 			<h3 class="text-dark fw-bold mt-2 mb-2 me-5">New Expense</h3>
 		</div>
 		<div class="d-flex align-items-center">
-			<a href="./expense/list" class="btn btn-primary  ">
+			<a href="./expense/list" class="btn btn-primary mr-2">
 				<i class="flaticon-list"></i> Expense List
 			</a>
+			<button class="btn btn-primary" dmx-on:click="varCategoryID.setValue(repeatItem.CategoryID.value);modalAddItem.show()"><i class="flaticon-plus"></i> Add Item</button>
 		</div>
 	</div>
 </div>
@@ -26,115 +30,116 @@
 <div class="container-fluid">
 	<form class="form" method="post" is="dmx-serverconnect-form" id="CreateExpense" action="dmxConnect/api/Expense/createExpense.php"
 		dmx-on:success="CreateExpense.reset();scGetExpense.load();varCounter.setValue(1);scInvoiceID.load();notifies1.success(&quot;Expense Created Succesfully&quot;);scExpenseList.load()">
-		<div class="card card-custom card-stretch gutter-b mb-2">
-			<div class="card-body border-dark-75 rounded">
+		<div class="card card-custom card-stretch gutter-b mb-2 ">
+			<div class="card-body border-dark-75 rounded pb-0">
 				<div class="row mb-2 py-1 row rounded p-2">
-					<div class="col-lg-1 col-6">
+				
+					<div class="col-lg-3 col-6 pl-2">
 						<div class="mb-3">
 							<label class="form-label">Invoice</label>
-							<input type="number" step="0.1" name="InvoiceNumber" dmx-bind:value="scInvoiceID.data.getMaxInvoiceID.InvoiceID+1" class="form-control" />
+							<input type="number" step="0.1" name="InvoiceNumber" dmx-bind:value="scInvoiceID.data.getMaxInvoiceID.InvoiceID+1" class="form-control form-control-sm" />
 						</div>
 					</div>
 					<div class="col-lg-3 col-6">
 						<div class="mb-3">
-							<label class="form-label">Invoice Name:</label>
-							<input type="text" name="InvoiceName" class="form-control" placeholder="Invoice Name" />
-						</div>
-					</div>
-					<div class="col offset-lg-6 offset-md-3">
-						<div class="mb-3">
 							<label class="form-label">Date:</label>
-							<input type="date" name="PurchaseDate" class="form-control" placeholder="Purchase Date" dmx-bind:value="varDateTime.datetime.formatDate(&quot;yyyy-MM-dd&quot;)" />
+							<input type="date" name="PurchaseDate" class="form-control form-control-sm" placeholder="Purchase Date" dmx-bind:value="varDateTime.datetime.formatDate(&quot;yyyy-MM-dd&quot;)" />
 						</div>
 					</div>
-				</div>
-				<div class="border rounded-lg table-responsive my-5 bg-light-o-25">
-					<table class="table table-head-custom table-vertical-center">
-						<thead>
-							<tr>
-								<th>#</th>
-								<th class="text-truncate">New Item</th>
-								<th class="text-truncate">Search Item</th>
-								<th class="text-truncate">Quantity</th>
-								<th class="text-truncate">Unit</th>
-								<th class="text-truncate">Amount</th>
-								<th class="text-truncate">+/-</th>
-							</tr>
-						</thead>
-						<tbody is="dmx-repeat" id="repeatItems" dmx-bind:repeat="varCounter.value">
-							<td>{{$index + 1}}</td>
-							<td class="text-truncate"><input type="text" class="form-control" autocomplete="off" name="NewItem[]"></td>
-							<td class="text-truncate"><input class="form-control" name="ItemID[]" is="dmx-autocomplete" dmx-bind:data="scItemLists.data.getItems" optiontext="subcategory_name" optionvalue="id"></td>
-							<td class="text-truncate"><input type="number" value="1" step="0.1" id="Quantity" name="Quantity[]" class="form-control" placeholder="Quantity" /></td>
-							<td class="text-truncate"><select class="form-control" name="UnitID[]" style="width: 100% !important;" dmx-bind:options="scUnits.data.queryUnits" optiontext="UnitName" optionvalue="UnitID"
-									dmx-bind:value="scItemLists.data.getItems.where(`id`,ItemID.value,'==').values(`default_unit`)">
-									<option value="" selected disabled>Select</option>
-								</select></td>
-							<td class="text-truncate"><input type="number" id="Price" dmx-bind:value="scItemLists.data.getItems.where(`id`,ItemID.value,'==').values(`default_price`) * Quantity.value" step="0.1" name="Amount[]" class="form-control" placeholder="Amount" />
-							</td>
-							<td class="text-truncate">
-								<button class="btn btn-icon btn-light-primary me-2 btn-lg fw-500" dmx-on:click="varCounter.setValue(varCounter.value + 1)">
-									<i class="fa fa-plus"></i>
-								</button>
-								<button class="btn btn-icon btn-light-danger me-2 btn-lg fw-500" dmx-show="varCounter.value != 1" dmx-on:click="varCounter.setValue(varCounter.value - 1)">
-									<i class="fa fa-minus"></i>
-								</button>
-							</td>
-						</tbody>
-					</table>
-				</div>
-				<div class="row mb-2 py-1 row rounded p-2">
-					<div class="col-lg-2 col-6">
+					<div class="col-lg-3 col-6 pl-2">
 						<div class="mb-3">
 							<label class="form-label">Account</label>
-							<select class="form-control" name="AccountID" dmx-bind:options="scAccountList.data.queryAccountList" optiontext="bank_name + ' ' + account_number" optionvalue="id" value="2">
+							<select class="form-control form-control-sm" name="AccountID" id="AccountID" dmx-bind:options="scAccountList.data.queryAccountList" optiontext="bank_name + ' ' + account_number" optionvalue="id" value="2">
 								<option selected disabled value="">Account</option>
 							</select>
 						</div>
 					</div>
-					<div class="col-lg-2 col-6">
+					<div class="col-lg-3 col-6" dmx-hide="AccountID.value == 2">
 						<div class="mb-3">
 							<label class="form-label">Payment Type:</label>
-							<select class="form-control" name="PaymentMethod" dmx-bind:options="scPaymentMethods.data.queryPaymentMethods" optiontext="PaymentType" optionvalue="PaymentID" value="6">
+							<select class="form-control form-control-sm" name="PaymentMethod" dmx-bind:options="scPaymentMethods.data.queryPaymentMethods" optiontext="PaymentType" optionvalue="PaymentID" value="6">
 								<option selected disabled value="">Payment Method</option>
 							</select>
 						</div>
 					</div>
-					<div class="col-lg-8 col-8">
+				</div>
+				<div class="d-flex">
+					<div class="border rounded-lg table-responsive  bg-light-o-25">
+						<table class="table table-head-custom table-vertical-center mb-0 table-borderless">
+							<thead class="bg-primary-o-15 border-bottom">
+								<tr>
+									<th>#</th>
+									<th class="text-truncate">New Item</th>
+									<th class="text-truncate">Search Item</th>
+									<th class="text-truncate">Quantity</th>
+									<th class="text-truncate">Unit</th>
+									<th class="text-truncate">Amount</th>
+									<th class="text-truncate">+/-</th>
+								</tr>
+							</thead>
+							<tbody is="dmx-repeat" id="repeatItems" dmx-bind:repeat="varCounter.value" >
+								<td>{{$index + 1}}</td>
+								<td class="text-truncate"><input type="text" class="form-control form-control-sm" autocomplete="off" name="NewItem[]"></td>
+								<td class="text-truncate"><input class="form-control form-control-sm" name="ItemID[]" is="dmx-autocomplete" dmx-bind:data="scItemLists.data.getItems" optiontext="subcategory_name" optionvalue="id"></td>
+								<td class="text-truncate"><input type="number" value="1" step="0.1" id="Quantity" name="Quantity[]" class="form-control form-control-sm" placeholder="Quantity" /></td>
+								<td class="text-truncate"><select class="form-control form-control-sm" name="UnitID[]" style="width: 100% !important;" dmx-bind:options="scUnits.data.queryUnits" optiontext="UnitName" optionvalue="UnitID"
+										dmx-bind:value="scItemLists.data.getItems.where(`id`,ItemID.value,'==').values(`default_unit`)">
+										<option value="" selected disabled>Select</option>
+									</select></td>
+								<td class="text-truncate"><input type="number" id="Price" dmx-bind:value="scItemLists.data.getItems.where(`id`,ItemID.value,'==').values(`default_price`) * Quantity.value" step="0.1" name="Amount[]" class="form-control form-control-sm" placeholder="Amount" />
+								</td>
+								<td class="text-truncate">
+									<button class="btn btn-icon btn-sm btn-light-primary me-2 fw-500" dmx-on:click="varCounter.setValue(varCounter.value + 1)">
+										<i class="fa fa-plus"></i>
+									</button>
+									<button class="btn btn-icon btn-light-danger btn-sm  me-2  fw-500" dmx-show="varCounter.value != 1" dmx-on:click="varCounter.setValue(varCounter.value - 1)">
+										<i class="fa fa-minus"></i>
+									</button>
+								</td>
+							</tbody>
+							<tr class="bg-primary-o-15">
+								<td colspan="5" class="text-right">Total: </td>
+								<td class="text-right font-weight-bold">{{repeatItems.items.sum(`Price.value`).formatCurrency("₹ ", ".", ",", "2")}}</td>
+								<td></td>
+							</tr>
+						</table>
+					</div>
+				</div>
+				
+				<div class="row mb-2 py-1 mt-5 row rounded p-2">
+					<div class="col-lg-6 col-12" dmx-show="varInvoiceName.value">
+						<div class="mb-3">
+							<label class="form-label">Invoice Name:</label>
+							<input type="text" name="InvoiceName" class="form-control form-control-sm" placeholder="Invoice Name" />
+						</div>
+					</div>
+					<div class="col-lg-6 col-8" dmx-show="varRemark.value">
 						<div class="mb-3">
 							<label class="form-label">Remark:</label>
-							<textarea name="Remark" rows="1" class="form-control" placeholder="Enter full name"></textarea>
+							<textarea name="Remark" rows="1" class="form-control form-control-sm" placeholder="Enter full name"></textarea>
 						</div>
 					</div>
 				</div>
-				<div class="row mb-2 py-1 row rounded p-2">
+				<div class="row mb-2 py-1 row rounded p-2" dmx-show="varAttachment.value">
 					<div class="mb-3 col">
 						<label class="form-label">Receipt</label>
 						<input is="dmx-dropzone" id="targetFile" type="file" name="target_photo">
 					</div>
 				</div>
-			</div>
-		</div>
-		<div class="row mt-2">
-			<div class="col ">
-				<button class="btn btn-primary me-2 btn-lg fw-500" dmx-on:click="varCategoryID.setValue(repeatItem.CategoryID.value);modalAddItem.show()">Add Item</button>
-			</div>
-			<div class="col  text-right">
-				<button class="btn btn-icon btn-primary me-2 btn-lg fw-500" dmx-on:click="varCounter.setValue(varCounter.value + 1)">
-					<i class="fa fa-plus"></i>
-				</button>
-				<button class="btn btn-icon btn-primary me-2 btn-lg fw-500" dmx-show="varCounter.value != 1" dmx-on:click="varCounter.setValue(varCounter.value - 1)">
-					<i class="fa fa-minus"></i>
-				</button>
-			</div>
-		</div>
-		<div class="row mt-2">
-			<div class="col text-center h4 fw-bold total-amount">Total Amount: {{repeatItems.items.sum(`Price.value`)}}</div>
-		</div>
-
-		<div class="text-center border-0">
-			<button type="submit" class="btn btn-primary me-2 btn-lg fw-500" dmx-bind:disabled="state.executing">SUBMIT <span class="spinner-border spinner-border-sm" role="status" dmx-show="state.executing"></span>
+				<div class="d-flex mb-3 align-items-center justify-content-between" >
+				<div class="flex-column">
+				<span class="font-weight-bold mr-3">Add More Options: </span>
+					<button class="btn btn-light-primary font-weight-bold mr-3 btn-sm rounded-3" dmx-on:click="varInvoiceName.value ? varInvoiceName.setValue(0) : varInvoiceName.setValue(1)"><i class="flaticon-plus"></i> Invoice Name</button>
+					<button class="btn btn-light-success font-weight-bold mr-3 btn-sm rounded-3" dmx-on:click="varRemark.value ? varRemark.setValue(0) : varRemark.setValue(1)"><i class="flaticon-plus"></i> Remark</button>
+					<button class="btn btn-light-warning font-weight-bold mr-3 btn-sm rounded-3" dmx-on:click="varAttachment.value ? varAttachment.setValue(0) : varAttachment.setValue(1)"><i class="flaticon-plus"></i> Attachment</button></div>
+				<div class="flex-column">
+				<button type="submit" class="btn btn-primary me-2 btn-lg fw-500" dmx-bind:disabled="state.executing">SUBMIT <span class="spinner-border spinner-border-sm" role="status" dmx-show="state.executing"></span>
 			</button>
+			</div>
+					
+					
+				</div>
+			</div>
 		</div>
 	</form>
 	<div class="card card-custom card-stretch gutter-b mt-5">
@@ -217,14 +222,14 @@
 				<div class="modal-body">
 					<div class="mb-3">
 						<label class="form-label">Category:</label>
-						<select class="form-control" name="category_id" dmx-bind:options="scCategories.data.getCategories" optiontext="category_name" optionvalue="id"
+						<select class="form-control form-control-sm" name="category_id" dmx-bind:options="scCategories.data.getCategories" optiontext="category_name" optionvalue="id"
 							dmx-bind:value="scCategories.data.getCategories.where(`id`, varCategoryID.value, '==').values(`id`)">
 							<option selected disabled value="">Categories</option>
 						</select>
 					</div>
 					<div class="mb-3">
 						<label class="form-label">Item:</label>
-						<input type="text" name="subcategory_name" class="form-control" required>
+						<input type="text" name="subcategory_name" class="form-control form-control-sm" required>
 					</div>
 				</div>
 				<div class="modal-footer">
