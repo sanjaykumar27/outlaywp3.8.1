1c40e6dab021e0e47529c5e45d71f7c12b93f9e6
SLASHASH-05\yo <sanjaychaurasia@slashash.co>
2021-03-25 18:15:00 +0530

    ..

---
+++ dmxConnect/api/Investments/MutualFunds/getFundDetails.php
@@ -0,0 +1,257 @@
+<?php
+require('../../../../dmxConnectLib/dmxConnect.php');
+
+
+$app = new \lib\App();
+
+$app->define(<<<'JSON'
+{
+  "meta": {
+    "$_GET": [
+      {
+        "type": "text",
+        "options": {
+          "rules": {
+            "core:required": {}
+          }
+        },
+        "name": "foliocode"
+      }
+    ]
+  },
+  "exec": {
+    "steps": {
+      "name": "apiFundDetails",
+      "module": "api",
+      "action": "send",
+      "options": {
+        "url": "{{'https://www.quandl.com/api/v3/datasets/AMFI/'+$_GET.foliocode+'.json?api_key=uuN2KGBSriTRXRkcgmwe'}}",
+        "passErrors": false,
+        "schema": []
+      },
+      "output": true,
+      "meta": [
+        {
+          "type": "object",
+          "name": "data",
+          "sub": [
+            {
+              "type": "object",
+              "name": "dataset",
+              "sub": [
+                {
+                  "type": "number",
+                  "name": "id"
+                },
+                {
+                  "type": "text",
+                  "name": "dataset_code"
+                },
+                {
+                  "type": "text",
+                  "name": "database_code"
+                },
+                {
+                  "type": "text",
+                  "name": "name"
+                },
+                {
+                  "type": "text",
+                  "name": "description"
+                },
+                {
+                  "type": "text",
+                  "name": "refreshed_at"
+                },
+                {
+                  "type": "text",
+                  "name": "newest_available_date"
+                },
+                {
+                  "type": "text",
+                  "name": "oldest_available_date"
+                },
+                {
+                  "type": "array",
+                  "name": "column_names",
+                  "sub": [
+                    {
+                      "type": "text",
+                      "name": "$value"
+                    }
+                  ]
+                },
+                {
+                  "type": "text",
+                  "name": "frequency"
+                },
+                {
+                  "type": "text",
+                  "name": "type"
+                },
+                {
+                  "type": "boolean",
+                  "name": "premium"
+                },
+                {
+                  "type": "text",
+                  "name": "limit"
+                },
+                {
+                  "type": "text",
+                  "name": "transform"
+                },
+                {
+                  "type": "text",
+                  "name": "column_index"
+                },
+                {
+                  "type": "text",
+                  "name": "start_date"
+                },
+                {
+                  "type": "text",
+                  "name": "end_date"
+                },
+                {
+                  "type": "array",
+                  "name": "data",
+                  "sub": [
+                    {
+                      "type": "array",
+                      "name": "$value",
+                      "sub": [
+                        {
+                          "type": "text",
+                          "name": "$value"
+                        }
+                      ]
+                    }
+                  ]
+                },
+                {
+                  "type": "text",
+                  "name": "collapse"
+                },
+                {
+                  "type": "text",
+                  "name": "order"
+                },
+                {
+                  "type": "number",
+                  "name": "database_id"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "type": "object",
+          "name": "headers",
+          "sub": [
+            {
+              "type": "text",
+              "name": "allow"
+            },
+            {
+              "type": "text",
+              "name": "cache-control"
+            },
+            {
+              "type": "text",
+              "name": "content-encoding"
+            },
+            {
+              "type": "text",
+              "name": "content-length"
+            },
+            {
+              "type": "text",
+              "name": "content-security-policy"
+            },
+            {
+              "type": "text",
+              "name": "content-type"
+            },
+            {
+              "type": "text",
+              "name": "date"
+            },
+            {
+              "type": "text",
+              "name": "etag"
+            },
+            {
+              "type": "text",
+              "name": "pragma"
+            },
+            {
+              "type": "text",
+              "name": "referrer-policy"
+            },
+            {
+              "type": "text",
+              "name": "server"
+            },
+            {
+              "type": "text",
+              "name": "status"
+            },
+            {
+              "type": "text",
+              "name": "strict-transport-security"
+            },
+            {
+              "type": "text",
+              "name": "vary"
+            },
+            {
+              "type": "text",
+              "name": "x-cdn"
+            },
+            {
+              "type": "text",
+              "name": "x-content-type-options"
+            },
+            {
+              "type": "text",
+              "name": "x-frame-options"
+            },
+            {
+              "type": "text",
+              "name": "x-iinfo"
+            },
+            {
+              "type": "text",
+              "name": "x-rack-cors"
+            },
+            {
+              "type": "text",
+              "name": "x-ratelimit-limit"
+            },
+            {
+              "type": "text",
+              "name": "x-ratelimit-remaining"
+            },
+            {
+              "type": "text",
+              "name": "x-request-id"
+            },
+            {
+              "type": "text",
+              "name": "x-runtime"
+            },
+            {
+              "type": "text",
+              "name": "x-xss-protection"
+            }
+          ]
+        }
+      ],
+      "outputType": "object"
+    }
+  }
+}
+JSON
+);
+?>
--- investment/spa_mutualFunds.php
+++ investment/spa_mutualFunds.php
@@ -1,4 +1,5 @@
-<!-- Wappler include head-page="../index.php" appconnect="local" is="dmx-app" fontawesome_5="local" jquery_slim_35="local" moment_2="local with locales" bootstrap4="local" components="{dmxAutocomplete:{}}" id="MutualFunds" -->
+<!-- Wappler include head-page="../index.php" appconnect="local" is="dmx-app" fontawesome_5="local" jquery_slim_35="local" moment_2="local with locales" bootstrap4="local" components="{dmxAutocomplete:{},dmxBootstrap4TableGenerator:{}}" id="MutualFunds" -->
+<dmx-serverconnect id="scFundDetails" url="dmxConnect/api/Investments/MutualFunds/getFundDetails.php" noload dmx-param:foliocode="varFolioID.value"></dmx-serverconnect>
 <dmx-serverconnect id="scGetFundsList" url="dmxConnect/api/Investments/MutualFunds/getFundsList.php" dmx-param:query="fundQuery.value" noload></dmx-serverconnect>
 <dmx-value id="varFolioID"></dmx-value>
 <div class="subheader py-2 py-lg-4 subheader-solid" id="kt_subheader">
@@ -16,10 +17,103 @@
             </div>
             <div class="d-flex" dmx-show="scGetFundsList.data.getList.hasItems()">
                 <ul class="list-group w-100 mutual-fund-list" is="dmx-repeat" id="repeatFunds" dmx-bind:repeat="scGetFundsList.data.getList" key="fund_id">
-                    <li class="list-group-item mouse-pointer text-hover-primary" dmx-on:click="varFolioID.setValue(code);scGetFundsList.reset()">{{name}}</li>
+                    <li class="list-group-item mouse-pointer text-hover-primary" dmx-on:click="varFolioID.setValue(code);scGetFundsList.reset();scFundDetails.load({})">{{name}}</li>
                 </ul>
             </div>
             <button class="btn mt-2 btn-light-danger font-weight-bold float-right small btn-sm rounded-3" dmx-show="scGetFundsList.data.getList.hasItems()" dmx-on:click="scGetFundsList.reset()">Close</button>
+        </div>
+        <div class="col-12">
+            <div class="table-responsive">
+                <table class="table table-striped table-hover">
+                    <tbody dmx-generator="bs4table" dmx-populate="scFundDetails.data.apiFundDetails.data.dataset">
+                        <tr>
+                            <th>Id</th>
+                            <td dmx-text="scFundDetails.data.apiFundDetails.data.dataset.id"></td>
+                        </tr>
+                        <tr>
+                            <th>Dataset code</th>
+                            <td dmx-text="scFundDetails.data.apiFundDetails.data.dataset.dataset_code"></td>
+                        </tr>
+                        <tr>
+                            <th>Database code</th>
+                            <td dmx-text="scFundDetails.data.apiFundDetails.data.dataset.database_code"></td>
+                        </tr>
+                        <tr>
+                            <th>Name</th>
+                            <td dmx-text="scFundDetails.data.apiFundDetails.data.dataset.name"></td>
+                        </tr>
+                        <tr>
+                            <th>Description</th>
+                            <td dmx-text="scFundDetails.data.apiFundDetails.data.dataset.description"></td>
+                        </tr>
+                        <tr>
+                            <th>Refreshed at</th>
+                            <td dmx-text="scFundDetails.data.apiFundDetails.data.dataset.refreshed_at"></td>
+                        </tr>
+                        <tr>
+                            <th>Newest available date</th>
+                            <td dmx-text="scFundDetails.data.apiFundDetails.data.dataset.newest_available_date"></td>
+                        </tr>
+                        <tr>
+                            <th>Oldest available date</th>
+                            <td dmx-text="scFundDetails.data.apiFundDetails.data.dataset.oldest_available_date"></td>
+                        </tr>
+                        <tr>
+                            <th>Column names</th>
+                            <td dmx-text="scFundDetails.data.apiFundDetails.data.dataset.column_names"></td>
+                        </tr>
+                        <tr>
+                            <th>Frequency</th>
+                            <td dmx-text="scFundDetails.data.apiFundDetails.data.dataset.frequency"></td>
+                        </tr>
+                        <tr>
+                            <th>Type</th>
+                            <td dmx-text="scFundDetails.data.apiFundDetails.data.dataset.type"></td>
+                        </tr>
+                        <tr>
+                            <th>Premium</th>
+                            <td dmx-text="scFundDetails.data.apiFundDetails.data.dataset.premium"></td>
+                        </tr>
+                        <tr>
+                            <th>Limit</th>
+                            <td dmx-text="scFundDetails.data.apiFundDetails.data.dataset.limit"></td>
+                        </tr>
+                        <tr>
+                            <th>Transform</th>
+                            <td dmx-text="scFundDetails.data.apiFundDetails.data.dataset.transform"></td>
+                        </tr>
+                        <tr>
+                            <th>Column index</th>
+                            <td dmx-text="scFundDetails.data.apiFundDetails.data.dataset.column_index"></td>
+                        </tr>
+                        <tr>
+                            <th>Start date</th>
+                            <td dmx-text="scFundDetails.data.apiFundDetails.data.dataset.start_date"></td>
+                        </tr>
+                        <tr>
+                            <th>End date</th>
+                            <td dmx-text="scFundDetails.data.apiFundDetails.data.dataset.end_date"></td>
+                        </tr>
+                        <tr>
+                            <th>Data</th>
+                            <td dmx-text="scFundDetails.data.apiFundDetails.data.dataset.data"></td>
+                        </tr>
+                        <tr>
+                            <th>Collapse</th>
+                            <td dmx-text="scFundDetails.data.apiFundDetails.data.dataset.collapse"></td>
+                        </tr>
+                        <tr>
+                            <th>Order</th>
+                            <td dmx-text="scFundDetails.data.apiFundDetails.data.dataset.order"></td>
+                        </tr>
+                        <tr>
+                            <th>Database</th>
+                            <td dmx-text="scFundDetails.data.apiFundDetails.data.dataset.database_id"></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
         </div>
     </div>
 </div>
